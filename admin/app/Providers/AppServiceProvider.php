6f1e9a0bbedb1c1ebb57084c8d6c8a689ad2e801
dovezukatmn <dovezuka.tmn@ya.ru>
2026-02-18 17:14:13 +0500

    fix(prod): force https and root URL scheme

--- admin/app/Providers/AppServiceProvider.php
+++ admin/app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+            \Illuminate\Support\Facades\URL::forceRootUrl(config('app.url'));
+        }
     }
 }
